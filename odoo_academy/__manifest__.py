6448f270f447d58621b78706f1a8f1e74f54fe95
Saraaah9 <acevedodariana9@gmail.com>
2022-05-12 22:10:22 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,35 @@
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description':"""
+        Academy module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version':'0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+    
+    
+}
